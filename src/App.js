2df1604743444a71a794e6fc588b9bf7be5d2b87
Leszek Michno <leszek.michno@gmail.com>
2022-03-31 09:08:17 +0200

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Storage from "./Storage";
 
-function App() {
+
+
+const App = () => {
+
+  const tShirts = [
+    {
+      id: 1,
+      type: "damska XS",
+      initialState: 12,
+      income: "",
+    },
+    {
+      id: 2,
+      type: "damska S",
+      initialState: 10,
+      income: "",
+    },
+    {
+      id: 3,
+      type: "damska M",
+      initialState: 8,
+      income: "",
+    },
+    {
+      id: 4,
+      type: "damska L",
+      initialState: 12,
+      income: "",
+    },
+    {
+      id: 5,
+      type: "męska S",
+      initialState: 10,
+      income: "",
+    },
+    {
+      id: 6,
+      type: "męska M",
+      initialState: 8,
+      income: "",
+    },
+    {
+      id: 7,
+      type: "męska L",
+      initialState: 10,
+      income: "",
+    },
+    {
+      id: 8,
+      type: "męska XL",
+      initialState: 8,
+      income: "",
+    },
+    {
+      id: 9,
+      type: "męska XXL",
+      initialState: 4,
+      income: "",
+    },
+  ];
+
+  const [totaIncome, setTotalIncome] = useState(0);  
+
+
+  const allTshirts = tShirts.map((item) => <Storage key={item.id} item={item} />);
+
+  const handleTotalIncome = () => {
+    const allIncomes = tShirts.map((item) => item.income);
+    const total = allIncomes.reduce((a, b) => a + b, 0)
+    setTotalIncome(total);
+    
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Sprzedaż t-shirt'ów</h1>
+      <hr />
+      {allTshirts}
+      <button onClick={handleTotalIncome}>Podsumowanie</button>
+      <span> Razem: <strong>{totaIncome}</strong> zł</span><br />
+      <button onClick={window.print}>Zapamiętaj</button>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Storage.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const Storage = ({item}) => {
+  const [tS, setTS] = useState(item.initialState * 1);
+  const [soldNumber, setSoldNumber] = useState("");
+  const sold = (e) => setSoldNumber(e.target.value);
+
+  const handleChange = () => {
+    if (tS >= soldNumber) {
+  
+      setTS(tS - soldNumber);
+      setSoldNumber("");
+    }
+  };
+  
+  const income = (item.initialState - tS) * 60; 
+
+  return (
+    <div>
+      <label>
+        <p>
+          <strong> {item.type} </strong> (stan początkowy: {item.initialState}
+          )
+        </p>
+        <span>
+          {" "}
+          stan obecny: <strong>{tS}</strong> {" "}
+        </span>
+        <input
+          className={soldNumber > 0 ? "blue" : " "}
+          type="number"
+          placeholder="sprzedane"
+          value={soldNumber}
+          onChange={sold}
+        />
+        <button onClick={handleChange}>Podlicz</button>
+        <span> {tS === "" ? 0 : `dochód: ${item.income = income} zł`}</span>
+        
+      </label>
+      <hr />
+    </div>
+  );
+};
+
+export default Storage;
